a4e30a5bf8db7f52cfb4636b192103d1cd0b2837
Abhinay Jangde <abhinayjangde@gmail.com>
2024-12-28 08:48:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+process.loadEnvFile()
+const port = process.env.PORT ?? 3001
+
+app.get("/",(req,res)=>{
+    console.log(req)
+    res.json({
+        msg:"namaste",
+        success:true
+    })
+})
+
+app.get("/html",(req,res)=>{
+    res.send(`
+        <div>
+        <button>click</button>
+        <p>abhinay</p>
+        </div>
+    `)
+})
+
+app.listen(port,()=>{
+    console.log(`Server listening at http://localhost:${port}`)
+})
