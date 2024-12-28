9cb8b2315fddc87940fec29c2da042a7da6018ab
Abhinay Jangde <abhinayjangde@gmail.com>
2024-12-28 09:04:33 +0530

    new route for user data

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT ?? 3001
 
+const data = {
+    username:"Abhi",
+    age:8,
+    email:"temp@email.com",
+    isActive:false
+}
 app.get("/",(req,res)=>{
     console.log(req)
     res.json({
@@ -18,6 +24,10 @@ app.get("/html",(req,res)=>{
         <p>abhinay</p>
         </div>
     `)
+})
+
+app.get("/user",(req,res)=>{
+    res.json(data)
 })
 
 app.listen(port,()=>{
